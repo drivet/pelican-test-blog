90cf51eef83903d62e94ebe834b78a20b9ab45a1
Desmond Rivet <desmond.rivet@gmail.com>
2019-09-15 17:42:34 -0400

    Add pelican-micropub plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,6 +33,8 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+PLUGINS = ['pelican_micropub']
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
